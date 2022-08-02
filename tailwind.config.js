86509185ce515c211074418d6c81b2cd669c869d
diegodvfe <diego.lopez.19@hotmail.es>
2022-08-01 20:15:47 -0800

    proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,18 @@ module.exports = {
     screen:{
       sm:'320px',
       md: '768px',
-      lg: '976px',
+      lg: '1020px',
       xl: '1440px'
     },
     extend: {
       color: {
-        strongCyan : 'hsl(171, 66%, 44%)',
+        strongCyan: 'hsl(171, 66%, 44%)',
         lightBlue: 'hsl(233, 100%, 69%)',
         darkGrayishBlue: 'hsl( 210, 10%, 33%)',
-        grayisBlue: 'hsl(201, 11%, 66%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-sarif']
       }
     },
   },
